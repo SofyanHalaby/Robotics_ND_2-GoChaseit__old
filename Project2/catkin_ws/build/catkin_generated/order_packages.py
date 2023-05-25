7919e8ffe38600fd5c3ad1295b9b0077c8bb8ed9
Sofyan Gaber <SofyanHalaby@gmail.com>
2023-05-25 05:14:57 -0700

    noneed

--- Project2/catkin_ws/build/catkin_generated/order_packages.py
+++ Project2/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/robond/ws/repos/Robotics_ND_2-GoChaseit/Project2/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/robond/ws/repos/Robotics_ND_2-GoChaseit/Project2/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/robond/ws/repos/Robotics_ND_2-GoChaseit/Project2/catkin_ws/devel;/opt/ros/kinetic" != "" else []
